c9a634861578686a1937c4e017078a7b49ab218a
unknown <hfigueroa1@udi.edu.co>
2020-06-02 17:53:47 -0500

    hfigueroa1 edito los controladores

--- src/Supermarket.API/Controllers/ValuesController.cs
+++ src/Supermarket.API/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Supermarket.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("hfigueroa1/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
@@ -21,25 +21,28 @@ namespace Supermarket.API.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return String.Format("Get individual al valor {0}",id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public string Post([FromBody] string value)
         {
+            return String.Format("Post individual al valor {0}",value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public string Put(int id, [FromBody] string value)
         {
+            return String.Format("Put individual al valor {0}",value);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public string Delete(int id)
         {
+            return String.Format("Delete individual al valor {0}",id);
         }
     }
 }
