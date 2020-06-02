0d1dd1828ee215384b4189ed1f6fcf02e3207c1f
stevenor-7 <eoviedo1@udi.edu.co>
2020-06-01 19:28:38 -0500

    Correccion de Errores del archivo ValuesController.cs

--- src/Supermarket.API/Controllers/ValuesController.cs
+++ src/Supermarket.API/Controllers/ValuesController.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Supermarket.eoviedo1.Controllers
+namespace Supermarket.API.Controllers
 {
     [Route("eoviedo1/[controller]")]
-    [eoviedo1Controller]
+    [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET eoviedo1/values
