879e6f92aa6c510b1dcb66d0d8de5e80635cb13d
david duran <jduran9@udi.edu.co>
2020-06-01 20:02:53 -0500

    se modiffico el controlador http con los verbos y se hicieron pruebas con el postman

--- src/Supermarket.API/Controllers/ValuesController.cs
+++ src/Supermarket.API/Controllers/ValuesController.cs
@@ -6,40 +6,44 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Supermarket.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("jduran9/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        // GET jduran9/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET jduran9/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return String.Format("get: Es utilizado únicamente para consultar información al servidor, muy parecidos a realizar un SELECT a la base de datos. {0}", id);
         }
 
-        // POST api/values
+        // POST jduran9/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public string Post([FromBody] string value)
         {
+          return String.Format("post: Es utilizado para solicitar la creación de un nuevo registro, es decir, algo que no existía previamente, es decir, es equivalente a realizar un INSERT en la base de datos. {0}", value);
         }
 
-        // PUT api/values/5
+        // PUT jduran9/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public string Put(int id, [FromBody] string value)
         {
+            return String.Format($"put: Se utiliza para actualizar por completo un registro existente, es decir, es parecido a realizar un UPDATE a la base de datos. {id}  algo enviado en el json {value}");
         }
 
-        // DELETE api/values/5
+        // DELETE jduran9/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public string Delete(int id)
         {
+          return String.Format("DELETE: Este método se utiliza para eliminar un registro existente, es similar a DELETE a la base de datos {0}", id);
+
         }
     }
 }
