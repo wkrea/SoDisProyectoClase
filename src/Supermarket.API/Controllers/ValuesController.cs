6ce775bf0ac4392fb5cc8a9fe1fca25863b12e70
kamikazeloco <dtorres10@udi.edu.co>
2020-06-01 19:08:31 -0500

    cambio de route y agregar mensajes en los metodos del HTTP

--- src/Supermarket.API/Controllers/ValuesController.cs
+++ src/Supermarket.API/Controllers/ValuesController.cs
@@ -6,40 +6,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Supermarket.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("dtorres10/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        // GET dtorres10/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET dtorres10/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return String.Format("get: Es utilizado únicamente para consultar información al servidor", id);
         }
 
-        // POST api/values
+        // POST dtorres10/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            return String.Format("post: Es utilizado para solicitar la creación de un nuevo registro, es decir, algo que no existía previamente.", value);
         }
 
-        // PUT api/values/5
+        // PUT dtorres10/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            return String.Format("put: Se utiliza para actualizar por completo un registro existente.");
         }
 
-        // DELETE api/values/5
+        // DELETE dtorres10/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            return String.Format("DELETE: Este método se utiliza para eliminar un registro existente.", id);
         }
     }
 }
