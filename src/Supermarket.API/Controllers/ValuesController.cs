0a28db562641e00ebc249665ba067a5890478424
kamikazeloco <dtorres10@udi.edu.co>
2020-06-05 03:02:30 -0500

    Modificacion de ValuesController.css a cada elemento

--- src/Supermarket.API/Controllers/ValuesController.cs
+++ src/Supermarket.API/Controllers/ValuesController.cs
@@ -21,28 +21,28 @@ namespace Supermarket.API.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return String.Format("get: Es utilizado únicamente para consultar información al servidor", id);
+            return String.Format("get: Es utilizado únicamente para consultar información al servidor {0}", id);
         }
 
         // POST dtorres10/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
-            return String.Format("post: Es utilizado para solicitar la creación de un nuevo registro, es decir, algo que no existía previamente.", value);
+            return String.Format("post: Es utilizado para solicitar la creación de un nuevo registro, es decir, algo que no existía previamente. {0}", value);
         }
 
         // PUT dtorres10/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
-            return String.Format("put: Se utiliza para actualizar por completo un registro existente.");
+            return String.Format("put: Se utiliza para actualizar por completo un registro existente. {0}, con el registro {1}", id, value);
         }
 
         // DELETE dtorres10/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            return String.Format("DELETE: Este método se utiliza para eliminar un registro existente.", id);
+            return String.Format("DELETE: Este método se utiliza para eliminar un registro existente.{0}", id);
         }
     }
 }
