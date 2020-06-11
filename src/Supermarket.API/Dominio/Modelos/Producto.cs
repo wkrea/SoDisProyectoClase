aeb391bc6d83c76e1fd5694abb46d9a7beafac7a
malliwi88 account <malliwi88@hotmail.com>
2020-06-11 14:22:36 -0500

    Actualización: * Documentación de las clases utilizadas.

--- src/Supermarket.API/Dominio/Modelos/Producto.cs
+++ src/Supermarket.API/Dominio/Modelos/Producto.cs
@@ -1,13 +1,49 @@
 ﻿namespace Supermarket.API.Dominio.Modelos
 {
+    /// <summary>
+    /// Esta clase define la estructura de datos 
+    /// que representa la entidad Producto en la base de datos
+    /// Permite el acceso a la información de la categoría
+    /// con la que está relacionada el producto, mediante:
+    /// 
+    /// La relación:
+    ///     Producto -> Categoria
+    /// Filtrando por:
+    ///     categoriasId
+    /// 
+    /// </summary>
     public class Producto
     {
-        public int id { get; set; }
-        public string nombre { get; set; }
-        public int cantidadxPaquete { get; set; }
-        // public EUnidadDeMedida unidadDeMedida {get; set;}
+        # region Atributos de Entidad Producto
+        /// <summary>
+        /// Propiedad de identificador con sus metodos de acceso para los productos
+        /// </summary>
+        /// <value></value>
+        public int id {get; set;}
+        /// <summary>
+        /// Propiedad Nombre con sus metodos de acceso para los productos
+        /// </summary>
+        /// <value></value>
+        public string nombre {get; set;}
+        public int cantidasxPaquete {get; set;}      
 
-        public int categoriaId { get; set; }
-        public Categoria categoria  { get; set; }
+        // public EUnidadDeMedida unidadDeMedida {get; set;}
+        #endregion
+
+
+        #region Relación Productos -> Categoría
+        /// <summary>
+        /// Llave foránea en tabla Categorías
+        /// </summary>
+        /// <value></value>
+        public int categoriaId {get; set;}
+        /// <summary>
+        /// Registros obtenidos al filtrar teniendo en cuenta
+        /// el valor que toma la llave foránea categoriaId
+        /// </summary>
+        /// <value></value>
+        public Categoria categoria {get; set;}   
+
+        #endregion
     }
 }
--- src/Supermarket.API/Dominio/Repositorios/ICategoriaRepo.cs
+++ src/Supermarket.API/Dominio/Repositorios/ICategoriaRepo.cs
@@ -7,17 +7,31 @@ namespace Supermarket.API.Dominio.Repositorios
 {
     /// <summary>
     /// Interfaz (Api Contract)
-    /// Permite definir los métodos que permitiran acceder la lógica de negocio
+    /// Permite definir los métodos que permitirán acceder la lógica de negocio
     /// Aislando la capa de acceso a datos de los demás módulos que consumen los
     /// datos
     /// </summary>
     public interface ICategoriaRepo
     {
         /// <summary>
-        /// Permite obtener la lista de categorias de forma asincrona
-        /// async y await
+        /// Método sincrono (secuencial)
+        /// Permite obtener la lista de categorías desde la base
         /// </summary>
         /// <returns></returns>
-        Task<IEnumerable<Categoria>> ListAsync();
+        IEnumerable<Categoria> GetCategorias();
+        /// <summary>
+        /// Método Asíncrono
+        /// Permite obtener la lista de categorías desde la base 
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Categoria>> GetCategoriasAsync();
+
+        /// <summary>
+        /// Permite obtener la información de la categoría asociada al 
+        /// identificador pasado por parametro
+        /// </summary>
+        /// <param name="id">Identificador de la categoría</param>
+        /// <returns></returns>
+        Categoria GetCategoriasAsyncById(int id);
     }
 }
