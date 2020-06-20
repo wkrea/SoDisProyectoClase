d6abc25ac6a2fa7bd84b556375ac84a3b380ffbe
Heiman Daza Tamayo <heimandaza@gmail.com>
2020-06-20 13:34:35 -0500

    hdaza1 avances ultimo parcial

--- src/Supermarket.API/Dominio/Repositorios/CategoriaRepo.cs
+++ src/Supermarket.API/Dominio/Repositorios/CategoriaRepo.cs
@@ -20,6 +20,49 @@ namespace Supermarket.API.Dominio.Repositorios
             db = Apicontext;
         }
 
+
+        /// <summary>
+        /// implenetacion de metodo que nos permite crear nuevas categorias
+        /// </summary>
+        /// <param name="categoria">entidad con los datos nuevos</param>
+        public void crearCategoria(Categoria categoria)
+        {
+            db.categorias.AddAsync(categoria);
+        }
+
+        /// <summary>
+        /// implementacion de metodo secuencial para editar categorias existentesen la db
+        /// </summary>
+        /// <param name="id">id del elemento que se desea modoficar</param>
+        /// <param name="categoria">entidad que tendra los nuevos registros</param>
+        public void editarCategoria(int id, Categoria categoria)
+        {
+           db.Entry(categoria).State = EntityState.Modified;
+           db.categorias.Update(categoria);
+        }
+
+        /// <summary>
+        /// Implemetacion  metodo secuencial para eliminar categoria
+        /// </summary>
+        /// <param name="categoria">Entidad que queremos eliminar de la base</param>
+        public void eliminarCategoria(Categoria categoria)
+        {
+            db.categorias.Remove(categoria);
+        }
+
+        /// <summary>
+        /// metodo que nos permite guardar datos en la base de datos
+        /// Implementacion metodo Asincrono --> usa paralelismo en el servidor
+        /// se retorna la categoria cuando este seguro del guardado asincrono sea exitoso
+        /// </summary>
+        /// <param name="categoria">Entidad que queremos guardar en la base</param>
+        /// <returns></returns>
+        public async Task<Categoria> guardarCategoria(Categoria categoria)
+        {
+            await db.SaveChangesAsync(); // commit
+            return categoria;
+        }
+
         /// <summary>
         /// Implemetacion  metodo secuencial
         /// </summary>
@@ -31,21 +74,25 @@ namespace Supermarket.API.Dominio.Repositorios
         //}
 
         /// <summary>
+        /// metodo que permite obtener la lista de categorias
         /// Implementacion metodo Asincrono --> usa paralelismo en el servidor
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Lista de categorias</returns>
         public async Task<IEnumerable<Categoria>> GetCategoriasAsync()
         {
             IEnumerable<Categoria> lista = await db.categorias.ToListAsync();
             return lista;
         }
 
-        public async Task<Categoria> FindCategoriasById(int id)
+        /// <summary>
+        /// metodo que nos permite tener una lista especifica
+        /// </summary>
+        /// <param name="id">id del elemento que se desea consultar</param>
+        /// <returns></returns>
+        public async Task<Categoria> GetCategoriasAsyncById(int id)
         {
             Categoria resultado = await db.categorias.FindAsync(id);
             return resultado;
         }
-
-
     }
 }
