db9d551f8780ae0d94fdffdaf0f46428fb71521e
ricardo1204 <nmaldonado1@udi.edu.co>
2020-06-20 22:08:36 -0500

    Se crearon los metodos para crear, editar y eliminar categorias

--- src/Supermarket.API/Dominio/Repositorios/CategoriaRepo.cs
+++ src/Supermarket.API/Dominio/Repositorios/CategoriaRepo.cs
@@ -15,6 +15,28 @@ namespace Supermarket.API.Dominio.Repositorios
             /// Acceso a toda la base de datos 
             db = apiContext;
         }
+        ///Creamos una categoría, el metodo debe ser asincrono debido a las validaciones necesarias del proceso
+         public void crearCategoria(Categoria categoria)
+        {
+            db.categorias.AddAsync(categoria);
+        }
+        ///Editamos la categoría reteniendo las llaves primarias y tomando lo que suministramos en el Update 
+        public void editarCategoria(int id, Categoria categoria)
+        {
+            db.Entry(categoria).State= EntityState.Modified;
+            db.categorias.Update(categoria);
+        }
+        ///El metodo recibe la categoría y la borra,  no es asincrono por que la eliminación no afecta la ejecución de la App desde aquí
+        public void eliminarCategoria(Categoria categoria)
+        {
+            db.categorias.Remove(categoria);
+        }
+        ///El metodo recibe la categoría a guardar y la retorna sólo si el guardado es exitoso 
+        public async Task<Categoria> guardarCategoria(Categoria categoria)
+        {
+            await db.SaveChangesAsync();
+            return categoria;
+        }
         /// <summary>
         /// Implementación secuencial sincrona
         /// </summary>
@@ -37,7 +59,7 @@ namespace Supermarket.API.Dominio.Repositorios
         /// <summary>
         /// Metodo que encuentrala categiróa dado un Id, la tarea se hace de forma asincrona 
         /// </summary>
-        public async Task<Categoria> FindCategoriaById(int id)
+        public async Task<Categoria> GetCategoriasAsyncById(int id)
         {
             /// utilizo el metodo del apiContext DbSet, le pasamos el id y él lo busca en la BD
             return await db.categorias.FindAsync(id);
